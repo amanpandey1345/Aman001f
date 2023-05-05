090edbd909312ca2e23620706c2ee71fe06350a8
amanpandey1345 <70140546+amanpandey1345@users.noreply.github.com>
2023-05-05 17:07:49 +0530

    fgdfggffd

--- backend/utils/jwtToken.js
+++ backend/utils/jwtToken.js
@@ -7,8 +7,7 @@ const sendToken = (user, statusCode, res)=>{
             Date.now() + process.env.COOKIE_EXPIRE * 24 * 60 * 100
         ),
         httpOnly:true,
-        secure: false,
-        sameSite: "none",
+
 
     }
     res.status(statusCode).cookie("token", token, options).json({
